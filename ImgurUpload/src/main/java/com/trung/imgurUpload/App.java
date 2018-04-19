96dbfa97b3e1ab669adbf15ae4fa235865b70cf1
trungnh103 <trungnh103@gmail.com>
2018-04-19 05:09:12 +0000

    App.java created online with Bitbucket

---
+++ ImgurUpload/src/main/java/com/trung/imgurUpload/App.java
@@ -0,0 +1,17 @@
+package com.trung.imgurUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Trung
+ * Starting point of the application.
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
